50f265accfe6a2ba849b37b2a95a72dadee664b9
GiovaForever96 <162158188+GiovaForever96@users.noreply.github.com>
2024-07-28 03:50:38 -0500

    Creación de plantilla base para control de activos

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  projectName: "Red Beneficiarios",
+  apiUrl: "http://localhost:5001/api/",
+  company: "Seguros Súarez",
+  website: "https://segurossuarez.com/",
+  mailAddress: "comunicaciones@segurossuarez.com",
+  copyright:
+    "Copyright © 2024 <b><a href='https://segurossuarez.com/' class='text-white' target='_blank'>Seguros Suárez</a></b> - Todos los derechos reservados.<b>Versión:</b> 1.0.0",
+  copyrightLogin:
+    "<b>Versión:</b> 1.0.0",
+  mapboxKey: 'pk.eyJ1IjoiZmVyNTE5NzQiLCJhIjoiY2t5bmdsYXV3MDMyazJycGRqcHNuMjZqZSJ9.XGjHdc-g6uY-j64MzL0Ejg'
+};
